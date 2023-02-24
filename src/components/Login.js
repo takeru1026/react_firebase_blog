2ba311d9b73bb71286bce2c0227f06189eb6276c
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-02-24 12:03:36 +0900

    :tada:ログイン機能の実装、ローカルストレージに情報をセット

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,6 @@ import {auth, provider } from "../firebase";
 import { useNavigate } from "react-router-dom";
 
 export const Login = ({ setIsAuth }) => {
-  // 
   const navigate = useNavigate();
   const loginWithGoogle = () => {
     // Google Login

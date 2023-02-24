8a0c12ccf68c920852121c0a2cf691bc1b8a3dba
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-02-24 12:49:36 +0900

    :tada:ファイルの作成、フォーマットの作成

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import "./CreatePost.css";
 
 export const CreatePost = () => {
   return (
-    <div>
-      CreatePost
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h2>記事を投稿する</h2>
+        <div className="inputPost">
+          <div>タイトル</div>
+          <input type="text" placeholder="タイトルを記入"/>
+        </div>
+        <div className="inputPost">
+          <div>投稿</div>
+          <textarea placeholder="投稿内容を記入"></textarea>
+        </div>
+        <button className="postButton">投稿する</button>
+      </div>
     </div>
   )
 }

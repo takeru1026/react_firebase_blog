f67d58b3d3ce2a034e0ef6b2ab87607a7f6713dc
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-02-24 15:12:39 +0900

    :tada:blogのデザイン修正

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "./Home.css"
 
 export const Home = () => {
   return (
-    <div>
-      Home
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          test. 頑張ります。っっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっk
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>name</h3>
+          <button>削除</button>
+        </div>
+      </div>
     </div>
   )
 }
